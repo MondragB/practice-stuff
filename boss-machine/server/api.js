4815a77dd83ce95f0ca51da8b3c6865b72201c9d
MondragB <bryantmndrgn16@hotmail.com>
2019-11-15 15:11:06 -0500

    Add two new routes for retrieving all minions and adding a new minion

--- boss-machine/server/api.js
+++ boss-machine/server/api.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+module.exports = apiRouter;`~`
+
+const { getAllFromDatabase, addToDatabase } = require('./db');
+
+apiRouter.get('/minions', (req, res, next) => {
+    res.send(getAllFromDatabase('minions'));
+});
+
+apiRouter.post('/minions', (req, res, next) => {
+    const newMinion = addToDatabase('minions', req.body);
+    res.status(201).send(newMinion);
+});
 
 
-module.exports = apiRouter;
+
+
+
