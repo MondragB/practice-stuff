f300f0f84fcf1200fbed61362bbbe1729ecfb86f
MondragB <bryantmndrgn16@hotmail.com>
2019-11-15 21:51:45 -0500

    Add route for updated a single minion, test passed

--- boss-machine/server/api.js
+++ boss-machine/server/api.js
@@ -3,7 +3,7 @@ const apiRouter = express.Router();
 
 module.exports = apiRouter;
 
-const { getAllFromDatabase, addToDatabase, getFromDatabaseById } = require('./db');
+const { getAllFromDatabase, addToDatabase, getFromDatabaseById, updateInstanceInDatabase } = require('./db');
 
 // Minion Routes
 apiRouter.get('/minions', (req, res, next) => {
@@ -18,7 +18,17 @@ apiRouter.post('/minions', (req, res, next) => {
 apiRouter.get('/minions/:minionId', (req, res, next) => {
     const minion = getFromDatabaseById('minions', req.params.minionId)
     if (minion) {
-        res.status(200).send(minion);
+        res.send(minion);
+    } else {
+        res.status(404).send();
+    }
+});
+
+apiRouter.put('/minions/:minionId', (req, res, next) => {
+    const minion = getFromDatabaseById('minions', req.params.minionId)
+    if (minion) {
+        let updatedMinion = updateInstanceInDatabase('minions', req.body);
+        res.send(updatedMinion);
     } else {
         res.status(404).send();
     }
