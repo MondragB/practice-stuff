fb38b7ce36c3c64f79e894acdba0184770a0e2e8
MondragB <bryantmndrgn16@hotmail.com>
2019-11-18 14:25:21 -0500

    Added all routes for Ideas path, all test passed

--- boss-machine/server/api.js
+++ boss-machine/server/api.js
@@ -49,7 +49,47 @@ apiRouter.delete('/minions/:minionId', (req, res, next) => {
 });
 
 // Ideas Route
+apiRouter.get('/ideas', (req, res, next) => {
+    res.send(getAllFromDatabase('ideas'));
+});
 
+apiRouter.post('/ideas', (req, res, next) => {
+    const newIdea = addToDatabase('ideas', req.body);
+    res.status(201).send(newIdea);
+});
+
+apiRouter.get('/ideas/:ideaId', (req, res, next) => {
+    const idea = getFromDatabaseById('ideas', req.params.ideaId)
+    if (idea) {
+        res.send(idea);
+    } else {
+        res.status(404).send();
+    }
+});
+
+apiRouter.put('/ideas/:ideaId', (req, res, next) => {
+    const idea = getFromDatabaseById('ideas', req.params.ideaId)
+    if (idea) {
+        let updatedIdea = updateInstanceInDatabase('ideas', req.body);
+        res.send(updatedIdea);
+    } else {
+        res.status(404).send();
+    }
+});
+
+apiRouter.delete('/ideas/:ideaId', (req, res, next) => {
+    const idea = getFromDatabaseById('ideas', req.params.ideaId)
+    if (idea) {
+        let deletedIdea = deleteFromDatabasebyId('ideas', req.params.ideaId);
+        if (deletedIdea) {
+            res.status(204).send();
+        } else {
+            res.status(404).send();
+        }
+    } else {
+        res.status(404).send();
+    }
+});
 
 
 
